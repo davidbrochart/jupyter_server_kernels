b3b0558004f97c756c8fa37c146059df9da88ba3
David Brochart <david.brochart@gmail.com>
2022-12-01 11:52:36 +0100

    Add kernelspecs handler

--- jupyter_server_kernels/kernelspecs/handlers.py
+++ jupyter_server_kernels/kernelspecs/handlers.py
@@ -14,7 +14,7 @@ from tornado import web
 
 from jupyter_server.auth import authorized
 
-from jupyter_server.base.handlers import APIHandler
+from jupyter_server.base.handlers import APIHandler, JupyterHandler
 from jupyter_server.utils import ensure_async, url_path_join, url_unescape
 
 AUTH_RESOURCE = "kernelspecs"
@@ -99,6 +99,32 @@ class KernelSpecHandler(KernelSpecsAPIHandler):
         self.finish(json.dumps(model))
 
 
+class KernelSpecResourceHandler(web.StaticFileHandler, JupyterHandler):
+    SUPPORTED_METHODS = ("GET", "HEAD")
+    auth_resource = AUTH_RESOURCE
+
+    def initialize(self):
+        web.StaticFileHandler.initialize(self, path="")
+
+    @web.authenticated
+    @authorized
+    def get(self, kernel_name, path, include_body=True):
+        ksm = self.kernel_spec_manager
+        if path.lower().endswith(".png"):
+            self.set_header("Cache-Control", f"max-age={60*60*24*30}")
+        try:
+            self.root = ksm.get_kernel_spec(kernel_name).resource_dir
+        except KeyError as e:
+            raise web.HTTPError(404, "Kernel spec %s not found" % kernel_name) from e
+        self.log.debug("Serving kernel resource from: %s", self.root)
+        return web.StaticFileHandler.get(self, path, include_body=include_body)
+
+    @web.authenticated
+    @authorized
+    def head(self, kernel_name, path):
+        return self.get(kernel_name, path, include_body=False)
+
+
 # URL to handler mappings
 
 kernel_name_regex = r"(?P<kernel_name>[\w\.\-%]+)"
@@ -106,4 +132,5 @@ kernel_name_regex = r"(?P<kernel_name>[\w\.\-%]+)"
 default_handlers = [
     (r"/api/kernelspecs", MainKernelSpecHandler),
     (r"/api/kernelspecs/%s" % kernel_name_regex, KernelSpecHandler),
+    (r"/kernelspecs/%s/(?P<path>.*)" % kernel_name_regex, KernelSpecResourceHandler),
 ]
